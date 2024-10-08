d3c62ba5460aed685355eeccf1af971dffc6183c
OngeraCodex <ongeradaryn@gmail.com>
2024-10-08 04:35:16 +0300

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -5,6 +5,7 @@ echo "Deploying the application..."
 # Simulate deployment steps
 # In a real deployment, you might copy files to a server or restart services
 echo "Copying files to the deployment directory..."
-# cp -r * /path/to/deployment/directory
+cp -r * /home/dongera/Azure_VM/project/directory/results.txt
+
 
 echo "Application deployed successfully."
